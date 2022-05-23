8e7cd2d65a9f44d912fe98b3bda2ba92d52c3403
G1gg1L3s <mr.a678i@gmail.com>
2022-05-23 23:29:03 +0300

    Add benches for soft-aes

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use diploma::hash::Sha256Builder;
+use diploma::{cipher::Aes128SafeBuilder, hash::Sha256Builder};
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn sha256(c: &mut Criterion) {
     let password = [0xde; 32];
     c.bench_function("sha256-hash-login-10000", |b| {
         b.iter(|| Sha256Builder::private_from_password(10000, password))
@@ -17,5 +17,24 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("sha256-hash-verify", |b| b.iter(|| public.verify_dry(&p1)));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn aes128_safe(c: &mut Criterion) {
+    let password = [0xde; 16];
+    let secret = [0xad; 16];
+    c.bench_function("aes128-soft-hash-login-10000", |b| {
+        b.iter(|| Aes128SafeBuilder::private_from_password(10000, secret, password))
+    });
+
+    let private = Aes128SafeBuilder::new_private(5, secret);
+
+    let p0 = private.get_password().unwrap();
+    let public = Aes128SafeBuilder::new_public(secret, p0);
+
+    let p1 = private.get_password().unwrap();
+
+    c.bench_function("aes128-soft-hash-verify", |b| {
+        b.iter(|| public.verify_dry(&p1))
+    });
+}
+
+criterion_group!(benches, sha256, aes128_safe);
 criterion_main!(benches);
