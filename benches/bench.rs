595604abf40ea1ec99208e1378e42c12bbd031e4
G1gg1L3s <mr.a678i@gmail.com>
2022-05-25 00:52:21 +0300

    Add commitment benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,12 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use diploma::{
     cipher::{Aes128NiBuilder, Aes128SafeBuilder},
+    commitment::{
+        aes::{Aes128NiEncryptor, Aes128SafeEncryptor},
+        ed25519::Ed25519,
+        hash::Sha256,
+        PrivateKey, PublicKey,
+    },
     hash::Sha256Builder,
 };
 
@@ -53,5 +59,102 @@ fn verify(c: &mut Criterion) {
     group.bench_function("aes128-hardware", |b| b.iter(|| public.verify_dry(&p1)));
 }
 
-criterion_group!(benches, register, verify);
+fn commitment_register(c: &mut Criterion) {
+    let mut group = c.benchmark_group("commitment/register");
+
+    group.bench_function("ed25519", |b| {
+        b.iter(|| {
+            let private = PrivateKey::new(Ed25519);
+            let public = PublicKey::new(Ed25519, private.public());
+            black_box(public);
+        })
+    });
+
+    group.bench_function("aes128safe", |b| {
+        b.iter(|| {
+            let private = PrivateKey::new(Aes128SafeEncryptor);
+            let public = PublicKey::new(Aes128SafeEncryptor, private.public());
+            black_box(public);
+        })
+    });
+
+    group.bench_function("aes128ni", |b| {
+        b.iter(|| {
+            let private = PrivateKey::new(Aes128NiEncryptor);
+            let public = PublicKey::new(Aes128NiEncryptor, private.public());
+            black_box(public);
+        })
+    });
+
+    group.bench_function("sha256", |b| {
+        b.iter(|| {
+            let private = PrivateKey::new(Sha256);
+            let public = PublicKey::new(Sha256, private.public());
+            black_box(public);
+        })
+    });
+}
+
+fn commitment_login(c: &mut Criterion) {
+    let mut group = c.benchmark_group("commitment/login");
+
+    let mut private = PrivateKey::new(Ed25519);
+    let mut public = PublicKey::new(Ed25519, private.public());
+    group.bench_function("ed25519", |b| {
+        b.iter(|| {
+            let reveal = private.private();
+            public.verify(&reveal);
+
+            private.advance();
+            public.advance(private.public());
+        })
+    });
+
+    let mut private = PrivateKey::new(Aes128SafeEncryptor);
+    let mut public = PublicKey::new(Aes128SafeEncryptor, private.public());
+
+    group.bench_function("aes128safe", |b| {
+        b.iter(|| {
+            let reveal = private.private();
+            public.verify(&reveal);
+
+            private.advance();
+            public.advance(private.public());
+        })
+    });
+
+    let mut private = PrivateKey::new(Aes128NiEncryptor);
+    let mut public = PublicKey::new(Aes128NiEncryptor, private.public());
+
+    group.bench_function("aes128ni", |b| {
+        b.iter(|| {
+            let reveal = private.private();
+            public.verify(&reveal);
+
+            private.advance();
+            public.advance(private.public());
+        })
+    });
+
+    let mut private = PrivateKey::new(Sha256);
+    let mut public = PublicKey::new(Sha256, private.public());
+
+    group.bench_function("sha256", |b| {
+        b.iter(|| {
+            let reveal = private.private();
+            public.verify(&reveal);
+
+            private.advance();
+            public.advance(private.public());
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    register,
+    verify,
+    commitment_register,
+    commitment_login
+);
 criterion_main!(benches);
